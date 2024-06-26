f635968779060e08e36811047d5fb2f1be5667a1
Sep Aminian <a.aminian1377@gmail.com>
2024-06-26 13:43:43 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -66,7 +66,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/thisissepehr"
+                  href="https://github.com/sephml"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
